caea61179c3990686c007e88d25f17f9d8d895e1
shiv shankar kumar <kumarshiv417@gmail.com>
2025-10-23 01:17:29 +0530

    reset button added

--- 01CounterApp/src/App.jsx
+++ 01CounterApp/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-
+import { RiResetRightLine } from "react-icons/ri";
 
 function App() {
 const [counter, setCounter] = useState(0);
@@ -20,9 +20,16 @@ const removeValue = () => {
     alert("You have reached to zero please press on Add button !")
   }
 }
+
+const resetButton = () => {
+  setCounter(0)
+}
   return (
     <>
     <div style={{color:"white",boxShadow:"5px 5px 25px gray",borderRadius:"10px",background:"black", display:"flex", flexDirection:"column", width:"500px", justifyContent:"center",textAlign:"center"}}>
+      <div className="resetBtn" style={{position:"absolute",top:"24px",left:"24px",cursor:"pointer"}} onClick={resetButton}>
+      <RiResetRightLine />
+      </div>
     <h1>Counter App upto 20</h1>
 <h1>{counter}</h1>
 <button style={{cursor:"pointer",padding:"10px 0px",fontWeight:"bold"}} onClick={addValue}>Add +</button>
