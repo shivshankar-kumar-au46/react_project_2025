a8856c8ce7b104a0c05862242a00de8ecf1244ba
shiv shankar kumar <kumarshiv417@gmail.com>
2025-11-06 15:42:07 +0530

    created search component

--- weatherApp/live_Weather_app/src/App.jsx
+++ weatherApp/live_Weather_app/src/App.jsx
@@ -5,6 +5,13 @@ function App() {
 const [weather, setWeather] = useState(null);
 const [loading, setLoading] = useState(false);
 const [error, setError] = useState('');
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const API_URL = `https://api.openweathermap.org/data/2.5/weather`;
+
+const fetchWeather = async () => {
+  
+}
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-blue-100">
       <div className="bg-black/90 text-white rounded-lg shadow-lg p-8 max-w-md">
