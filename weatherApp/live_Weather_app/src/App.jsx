5b566a735a3479c28aefe6a8f6d7034273eadd23
shiv shankar kumar <kumarshiv417@gmail.com>
2025-11-11 01:51:31 +0530

    all UI completed and video added as bg

--- weatherApp/live_Weather_app/src/App.jsx
+++ weatherApp/live_Weather_app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import SearchBar from "./components/SearchBar"
 import axios from 'axios';
 import WeatherCard from "./components/WeatherCard";
+import video from "./video.mp4";
 
 function App() {
 const [weather, setWeather] = useState(null);
@@ -12,7 +13,6 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 const API_URL = `https://api.openweathermap.org/data/2.5/weather`;
 
 const fetchWeather = async (city) => {
-  console.log(city)
   setLoading(true);
   setError("");
   try {
@@ -28,13 +28,21 @@ const fetchWeather = async (city) => {
       setError("An error occurred. Please try again later.")
     }
     setWeather(null);
+  } finally {
+    setLoading(false);
   }
 }
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-blue-100">
-      <div className="bg-black/90 text-white rounded-lg shadow-lg p-8 max-w-md">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-blue-100 relative">
+      <video autoPlay loop muted className="absolute top-0 left-0 w-full h-full object-cover"> 
+        <source type="video/mp4" src={video}/>
+      </video>
+      <div className="h-full w-full absolute top-0 left-0 bg-black/30"></div>
+      <div className="bg-black/70 text-white rounded-lg shadow-lg p-8 max-w-md z-10">
 <h1 className="text-3xl font-bold text-center mb-6 ">Weather App</h1>
 <SearchBar fetchWeather={fetchWeather}/>
+{loading && <p className="text-center mt-4">Loading ...</p>}
+{error && <p className="text-red-500 text-center mt-4">{error}</p>}
 {weather && <WeatherCard weather={weather}/>}
       </div>
     </div>
--- weatherApp/live_Weather_app/src/components/WeatherCard.jsx
+++ weatherApp/live_Weather_app/src/components/WeatherCard.jsx
@@ -13,9 +13,31 @@ const WeatherCard = ({ weather }) => {
     />
     <p className='text-3xl font-bold'>{`${Math.round(weather.main.temp)} °C`}</p>
         </div>
-   
-    
-    <p className='text-center text-gray-400'>{weather.weather[0].description}</p>
+    <p className='text-center text-gray-400 mb-[21px] mt-[-13px]'>{weather.weather[0].description}</p>
+
+    <div className='w-full'>
+        <div className='flex justify-between'>
+            <div className='text-center'>
+            <p className='text-gray-400 text-sm'>Humidity</p>
+            <p>{weather.main.humidity} %</p>
+        </div>
+        <div className='text-center'>
+            <p className='text-gray-400 text-sm'>Wind Speed</p>
+            <p>{weather.wind.speed} m/s</p>
+        </div>
+        </div>
+        <div className='flex justify-between mt-4'>
+<div className='text-center'>
+            <p className='text-gray-400 text-sm'>Pressure</p>
+            <p>{weather.main.pressure} hpa</p>
+        </div>
+         <div className='text-center'>
+            <p className='text-gray-400 text-sm'>Feels Like</p>
+            <p>{weather.main.feels_like} °C</p>
+        </div>
+        </div>
+        
+    </div>
     </div>
   )
 }
